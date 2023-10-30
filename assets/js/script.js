82b80e9e9bca110213e4f30b25c38a41630d7226
sabri chafroud <chafroud.sabri@hotmail.fr>
2023-10-30 16:52:24 +0100

    30/10/23 16h53

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,52 @@ console.log("Script chargé");
 //
 //************************DÉCLARATION DES CONSTANTES ET VARIABLES**************************//
 //
-const ARRAY_MOTS = ["NAVIGATEUR", "RESPONSIVE", "PACKAGE", "PROJET", "REFERENCEMENT", "ALGORITHME", "FRAMEWORK", "PROCEDURALE", "APPLICATION"];
-const MOT_MYSTERE = ARRAY_MOTS[Math.floor(Math.random() * ARRAY_MOTS.length)];
+// const ARRAY_MOTS = ["NAVIGATEUR", "RESPONSIVE", "PACKAGE", "PROJET", "REFERENCEMENT", "ALGORITHME", "FRAMEWORK", "PROCEDURALE", "APPLICATION"];
+const ARRAY_DEFINITIONS = [
+    {
+        mot: "NAVIGATEUR",
+        definition: "Un navigateur web est un logiciel conçu pour consulter et afficher le World Wide Web"
+    },
+    {
+        mot: "RESPONSIVE",
+        definition: "Un site web responsive ou 'réactif' est un site web dont la conception vise à offrir une consultation confortable sur des écrans de tailles très différentes"},
+    {
+        mot: "PACKAGE",
+        definition: "En développement web, le terme package ou 'paquet' est le nom donné à une bibliothèque logicielle."},
+    {
+        mot: "PROJET",
+        definition: "Techniquement, le terme projet est la concrétisation ou la réalisation d'une idée en mobilisant les ressources nécessaires dont on a besoin, en fixant et en respectant le délai de sa réalisation sans oublier la qualité de la finalité."},
+    {
+        mot: "REFERENCEMENT",
+        definition: "Sur internet, le travail de référencement consiste à améliorer le positionnement et la visibilité de sites dans des pages de résultats de moteurs de recherche ou d'annuaires."},
+    {
+        mot: "ALGORITHME",
+        definition: "Un algorithme est une suite finie et non ambiguë d'instructions et d\'opérations permettant de résoudre une classe de problèmes."},
+    {
+        mot: "FRAMEWORK",
+        definition: "En développement web, un framework est un ensemble cohérent de composants structurels qui sert à créer l'architecture de tout ou partie d'une application"},
+    {
+        mot: "PROCEDURALE",
+        definition: "En informatique, la programmation procédurale est un paradigme qui se fonde sur le concept d'appel procédural. Une procédure, aussi appelée routine, sous-routine ou fonction (à ne pas confondre avec les fonctions de la programmation fonctionnelle reposant sur des fonctions mathématiques), contient simplement une série d'étapes à réaliser. N\'importe quelle procédure peut être appelée à n'importe quelle étape de l\'exécution du programme, y compris à l\'intérieur d\'autres procédures, voire dans la procédure elle-même (récursivité)."},
+    {
+        mot: "APPLICATION",
+        definition: "En informatique, une application web est une application manipulable directement en ligne grâce à un navigateur web et qui ne nécessite donc pas d'installation sur les machines clientes, contrairement aux applications mobiles."}
+]
+
+let numAleatoire = Math.floor(Math.random() * ARRAY_DEFINITIONS.length);
+console.log(numAleatoire);
+
+const MOT_MYSTERE = ARRAY_DEFINITIONS[numAleatoire].mot;
+console.log("mot aleatoir " + MOT_MYSTERE);
+
+const definitionMotMystere = ARRAY_DEFINITIONS[numAleatoire].definition;
+console.log("definition aleatoir " + definitionMotMystere);
+
+
+
+// const MOT_MYSTERE = ARRAY_DEFINITIONS;
+console.log(Math.floor(Math.random() * ARRAY_DEFINITIONS.length));
+// console.log("test", ARRAY_DEFINITIONS[4]);
 console.log(MOT_MYSTERE);
 const ARRAY_ALPHABET = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 console.log(ARRAY_ALPHABET);
@@ -74,6 +118,9 @@ function chercher(evenementClick) {
             if (!gagne) {
                 document.querySelector(".resultat p").textContent = "GAGNÉ !";
                 document.querySelector(".interface").style.pointerEvents = "none";
+
+                afficheDefinitionMystere();
+                
                 BOUTON_REJOUER.style.visibility = "visible";
             }
         }
@@ -83,11 +130,23 @@ function chercher(evenementClick) {
         SPAN_SCORE.innerHTML = `<strong>${parseInt(SPAN_SCORE.textContent) - 1}</strong>`;
         if (SPAN_SCORE.textContent == 0) {
             document.querySelector(".resultat p").innerHTML = `PERDU ! Le mot à trouver était <strong>${MOT_MYSTERE}</strong>`;
+
+            afficheDefinitionMystere()
+
             document.querySelector(".interface").style.pointerEvents = "none";
             BOUTON_REJOUER.style.visibility = "visible";
+   
         }
     }
+    
+
     boutonLettre.style.color = "white";
     boutonLettre.disabled = true;
     trouve = false;
 }
+
+function afficheDefinitionMystere() {
+    document.querySelector(".resultat p:last-of-type").textContent = definitionMotMystere;
+}
+
+
